22c0b46dba49a820fc67b28c34269091dae36ea1
WebNiloy <niloyDev2004@gmail.com>
2024-07-02 20:18:54 +0600

    switch & case operation

--- script.js
+++ script.js
@@ -21,3 +21,24 @@ console.log(items.indexOf("mug"));
 console.log(items.push("laptop","iphone"));
 console.log(items);
 console.log(items.pop());
+
+// === switch & case operation
+const color = "green";
+switch (color) {
+    case "red":
+        console.log("color is red");
+        break;
+    case "gary":
+        console.log("color is gary");
+        break;
+    case "green":
+        console.log("color is green");
+        break;
+    case "yellow":
+        console.log("color is yellow");
+        break;
+
+    default:
+        console.log("color is black");
+        break;
+}
